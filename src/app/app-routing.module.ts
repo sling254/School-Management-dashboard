34c74dcf15715a03c9cbf058ec1d9fc8d5c0f0c3
sling254 <mwanikidenis7@gmail.com>
2021-11-30 21:52:18 +0300

    add a dashboard

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,13 @@ import { TablesComponent } from './components/tables/tables.component';
 import { NotfoundComponent } from './components/notfound/notfound.component';
 import { StudentManagerComponent } from './components/student-manager/student-manager.component';
 import { CourseComponent } from './components/course/course.component';
-
 import { TrainersComponent } from './components/trainers/trainers.component';
+import{ DashboardComponent } from './components/dashs/dashboard.component';
 
 
 const routes: Routes = [
-  {path: 'students',component:StudentManagerComponent},
+  {path: 'dash',component:DashboardComponent},
+  {path: 'students',component:StudentManagerComponent}, 
   {path: 'courses',component:CourseComponent},
   {path: 'trainers',component:TrainersComponent},
   {path:"**", component:NotfoundComponent}
